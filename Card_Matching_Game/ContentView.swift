d4ace8bd911fec55dba8acd173348f391ea67200
Raza <rtshareef@gmail.com>
2020-08-19 17:13:52 -0400

    front/back

--- Card_Matching_Game/ContentView.swift
+++ Card_Matching_Game/ContentView.swift
@@ -12,14 +12,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        return ZStack(content:{
-            RoundedRectangle(cornerRadius: 10.0).stroke()
-         Text("🐙")
-            }).foregroundColor(Color.orange).padding()
+        return HStack(spacing:1.5,content:{
+//            HStack puts everything in one view-left to right
+            ForEach(0..<4,content: {index in
+                CardView(isFaceUp: false)
+        
+            })
+        
+        }).foregroundColor(Color.orange).padding().font(Font.largeTitle)
         
     }
 }
 
+
+
+struct CardView: View{
+    var isFaceUp: Bool
+    
+    
+    var body: some View {
+        ZStack(content:{
+            if isFaceUp {
+            RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+            Text("🐙")
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        })
+        }
+}
 
 
 
